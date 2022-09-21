c33fb6762e151428d2d91ff0d462521062ea749f
namicswami <104111507+namicswami@users.noreply.github.com>
2022-09-21 13:29:13 -0400

    4 files modified, 1 added, 1 deleted

--- src/app/pages/app.crud.component.ts
+++ src/app/pages/app.crud.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-crud',
-  templateUrl: './crud.component.html',
-  styleUrls: ['./crud.component.css']
+  templateUrl: './app.crud.component.html',
+  styleUrls: ['./app.crud.component.scss']
 })
 export class AppCrudComponent implements OnInit {
 
